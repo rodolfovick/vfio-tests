97e6cef029eb5bc892472e68666ad12d63d6d603
Cédric Le Goater <clg@redhat.com>
2026-08-12 19:13:29 +0200

    run-test.sh: Detect VF/PF and adapt test parameters
    - Detect VF vs PF via the physfn sysfs symlink
    - Skip PF-only tests (huge-guest, hot-reset, huge-fault-race) on VFs
    - Read dma_entry_limit from sysfs to compute map size and iteration
      limits for IOMMU mapping tests
    - Print IOMMU address space width from the Intel IOMMU CAP register
    - Add migration stress test to the test run
    
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- run-test.sh
+++ run-test.sh
@@ -5,19 +5,52 @@ me=${0##*/}
 device=${1:-"0000:08:10.0"}
 groupid=$(basename $(readlink /sys/bus/pci/devices/$device/iommu_group))
 
+# Detect VF vs PF
+if [ -e /sys/bus/pci/devices/$device/physfn ]; then
+	is_vf=1
+	echo "Device $device is a Virtual Function"
+else
+	is_vf=0
+	echo "Device $device is a Physical Function"
+fi
+
+# IOMMU address space size from CAP register (MGAW field, bits 21:16)
+iommu_cap=$(cat /sys/bus/pci/devices/$device/iommu/intel-iommu/cap 2>/dev/null)
+if [ -n "$iommu_cap" ]; then
+	mgaw=$(( (0x$iommu_cap >> 16) & 0x3f ))
+	echo "IOMMU address space: $(( mgaw + 1 ))-bit"
+fi
+
+# DMA mapping limits from vfio_iommu_type1 dma_entry_limit
+pagesize=$(getconf PAGESIZE)
+dma_entry_limit=$(cat /sys/module/vfio_iommu_type1/parameters/dma_entry_limit 2>/dev/null)
+dma_entry_limit=${dma_entry_limit:-65535}
+# map-unmap uses page-sized chunks: max mappable = dma_entry_limit * pagesize
+map_size_mb=$(( dma_entry_limit * pagesize / 1024 / 1024 ))
+# stress-test uses 2MB chunks over 1GB iterations: 512 entries per iteration
+map_max=$(( dma_entry_limit / 512 ))
+[ $map_max -lt 4 ] && map_max=4
+echo "DMA entry limit: $dma_entry_limit, page size: ${pagesize} (page map: ${map_size_mb}MB, 2M map: ${map_max} iterations)"
+
 set -x
 
 ./vfio-correctness-tests $groupid
-./vfio-huge-guest-test $groupid
-./vfio-iommu-map-unmap $device
-./vfio-iommu-stress-test $device
+
+if [ $is_vf -eq 0 ]; then
+	./vfio-huge-guest-test $groupid
+	./vfio-pci-hot-reset $device
+	./vfio-pci-huge-fault-race $device
+fi
+
+./vfio-iommu-map-unmap $device $map_size_mb 5
+./vfio-iommu-stress-test $device $map_max
+
 ./vfio-noiommu-pci-device-open $device
 ./vfio-pci-device-dma-map $device
 ./vfio-pci-device-open $device
 ./vfio-pci-device-open-igd $device
 ./vfio-pci-device-open-sparse-mmap $device
-./vfio-pci-hot-reset $device
-./vfio-pci-huge-fault-race $device
 ./iommufd-pci-device-open $device
 ./vfio-pci-device-migration $device
+./vfio-pci-device-migration-stress $device
 ./vfio-pci-device-map-alignment $device
