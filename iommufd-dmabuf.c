4ff5e42b94a19a532a7cd60837f4b4173a4b85e9
Cédric Le Goater <clg@redhat.com>
2026-08-31 16:32:49 +0200

    iommufd-dmabuf: refactor BAR export into test_export_bars()
    Extract the dma-buf export ioctl into a try_dmabuf_export() helper
    and move the BAR export loop into test_export_bars() to prepare for
    adding negative test cases.
    
    No functional change.
    
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- iommufd-dmabuf.c
+++ iommufd-dmabuf.c
@@ -39,6 +39,104 @@ struct vfio_device_feature_dma_buf {
 };
 #endif
 
+static int try_dmabuf_export(int device, int region_index, __u64 length)
+{
+	struct {
+		struct vfio_device_feature hdr;
+		struct vfio_device_feature_dma_buf dma_buf;
+		struct vfio_region_dma_range range;
+	} dma_buf_feature = {
+		.hdr = {
+			.argsz = sizeof(dma_buf_feature),
+			.flags = VFIO_DEVICE_FEATURE_GET |
+				 VFIO_DEVICE_FEATURE_DMA_BUF,
+		},
+		.dma_buf = {
+			.region_index = region_index,
+			.open_flags = O_RDWR,
+			.nr_ranges = 1,
+		},
+		.range = {
+			.length = length,
+		},
+	};
+
+	return ioctl(device, VFIO_DEVICE_FEATURE, &dma_buf_feature);
+}
+
+static int test_export_bars(int src_device, int iommufd,
+			    const char *src_name, const char *dst_name,
+			    int dst_ioas)
+{
+	struct vfio_region_info region_info = { .argsz = sizeof(region_info) };
+	int i, ret;
+
+	printf("\nExporting %s BARs as dma-buf, mapping into %s ioas %d\n\n",
+	       src_name, dst_name ? dst_name : src_name, dst_ioas);
+
+	for (i = 0; i < VFIO_PCI_ROM_REGION_INDEX; i++) {
+		int dmabuf_fd;
+
+		region_info.index = i;
+		ret = ioctl(src_device, VFIO_DEVICE_GET_REGION_INFO,
+			    &region_info);
+		if (ret)
+			continue;
+
+		if (!region_info.size ||
+		    !(region_info.flags & VFIO_REGION_INFO_FLAG_MMAP))
+			continue;
+
+		printf("BAR%d: size 0x%lx, offset 0x%lx, flags 0x%x\n",
+		       i, (unsigned long)region_info.size,
+		       (unsigned long)region_info.offset, region_info.flags);
+
+		dmabuf_fd = try_dmabuf_export(src_device, i, region_info.size);
+		if (dmabuf_fd < 0) {
+			printf("\tdma-buf failed (%s)\n", strerror(errno));
+			continue;
+		}
+
+		printf("\texported as dma-buf fd %d\n", dmabuf_fd);
+
+		void *map = mmap(NULL, (size_t)region_info.size,
+				 PROT_READ, MAP_SHARED, src_device,
+				 (off_t)region_info.offset);
+		if (map == MAP_FAILED) {
+			printf("\tmmap failed (%s)\n", strerror(errno));
+		} else {
+			if (verbose)
+				hexdump(map, region_info.size > 64 ? 64 :
+					region_info.size);
+			munmap(map, (size_t)region_info.size);
+		}
+
+		struct iommu_ioas_map_file map_file = {
+			.size = sizeof(map_file),
+			.flags = IOMMU_IOAS_MAP_READABLE |
+				 IOMMU_IOAS_MAP_WRITEABLE,
+			.ioas_id = dst_ioas,
+			.fd = dmabuf_fd,
+			.length = region_info.size,
+		};
+		ret = ioctl(iommufd, IOMMU_IOAS_MAP_FILE, &map_file);
+		if (ret < 0) {
+			printf("\tFailed IOMMU_IOAS_MAP_FILE %d (%s)\n",
+			       ret, strerror(errno));
+			close(dmabuf_fd);
+			continue;
+		}
+
+		printf("\tmapped in %s ioas %d at IOVA 0x%llx size 0x%lx\n",
+		       dst_name ? dst_name : src_name, dst_ioas,
+		       (unsigned long long)map_file.iova,
+		       (unsigned long)region_info.size);
+		close(dmabuf_fd);
+	}
+
+	return 0;
+}
+
 void usage(char *name)
 {
 	printf("usage: %s [options] <ssss:bb:dd.f>\n", name);
@@ -51,10 +149,9 @@ void usage(char *name)
 int main(int argc, char **argv)
 {
 	const char *src_name, *dst_name = NULL;
-	int opt, i, src_device, dst_device, iommufd, ret;
+	int opt, src_device, dst_device, iommufd, ret;
 	int src_ioas, dst_ioas;
 	struct vfio_device_info device_info = { .argsz = sizeof(device_info) };
-	struct vfio_region_info region_info = { .argsz = sizeof(region_info) };
 
 	while ((opt = getopt(argc, argv, "d:h")) != -1) {
 		switch (opt) {
@@ -110,89 +207,10 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	printf("\nExporting %s BARs as dma-buf, mapping into %s ioas %d\n\n",
-	       src_name, dst_name ? dst_name : src_name, dst_ioas);
-
-	for (i = 0; i < VFIO_PCI_ROM_REGION_INDEX; i++) {
-		int dmabuf_fd;
-
-		region_info.index = i;
-		ret = ioctl(src_device, VFIO_DEVICE_GET_REGION_INFO,
-			    &region_info);
-		if (ret)
-			continue;
-
-		if (!region_info.size ||
-		    !(region_info.flags & VFIO_REGION_INFO_FLAG_MMAP))
-			continue;
-
-		printf("BAR%d: size 0x%lx, offset 0x%lx, flags 0x%x\n",
-		       i, (unsigned long)region_info.size,
-		       (unsigned long)region_info.offset, region_info.flags);
-
-		struct {
-			struct vfio_device_feature hdr;
-			struct vfio_device_feature_dma_buf dma_buf;
-			struct vfio_region_dma_range range;
-		} dma_buf_feature = {
-			.hdr = {
-				.argsz = sizeof(dma_buf_feature),
-				.flags = VFIO_DEVICE_FEATURE_GET |
-					 VFIO_DEVICE_FEATURE_DMA_BUF,
-			},
-			.dma_buf = {
-				.region_index = i,
-				.open_flags = O_RDWR,
-				.nr_ranges = 1,
-			},
-			.range = {
-				.length = region_info.size,
-			},
-		};
-
-		dmabuf_fd = ioctl(src_device, VFIO_DEVICE_FEATURE,
-				  &dma_buf_feature);
-		if (dmabuf_fd < 0) {
-			printf("\tdma-buf failed (%s)\n", strerror(errno));
-			continue;
-		}
-
-		printf("\texported as dma-buf fd %d\n", dmabuf_fd);
-
-		void *map = mmap(NULL, (size_t)region_info.size,
-				 PROT_READ, MAP_SHARED, src_device,
-				 (off_t)region_info.offset);
-		if (map == MAP_FAILED) {
-			printf("\tmmap failed (%s)\n", strerror(errno));
-		} else {
-			if (verbose)
-				hexdump(map, region_info.size > 64 ? 64 :
-					region_info.size);
-			munmap(map, (size_t)region_info.size);
-		}
-
-		struct iommu_ioas_map_file map_file = {
-			.size = sizeof(map_file),
-			.flags = IOMMU_IOAS_MAP_READABLE |
-				 IOMMU_IOAS_MAP_WRITEABLE,
-			.ioas_id = dst_ioas,
-			.fd = dmabuf_fd,
-			.length = region_info.size,
-		};
-		ret = ioctl(iommufd, IOMMU_IOAS_MAP_FILE, &map_file);
-		if (ret < 0) {
-			printf("\tFailed IOMMU_IOAS_MAP_FILE %d (%s)\n",
-			       ret, strerror(errno));
-			close(dmabuf_fd);
-			continue;
-		}
-
-		printf("\tmapped in %s ioas %d at IOVA 0x%llx size 0x%lx\n",
-		       dst_name ? dst_name : src_name, dst_ioas,
-		       (unsigned long long)map_file.iova,
-		       (unsigned long)region_info.size);
-		close(dmabuf_fd);
-	}
+	ret = test_export_bars(src_device, iommufd, src_name, dst_name,
+			       dst_ioas);
+	if (ret)
+		return ret;
 
 	close(src_device);
 	if (dst_name)
